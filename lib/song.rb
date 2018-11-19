2e6935f9ca90cebffd9065c55628553dea722b36
Leslie Osorio <lesosorio17@gmail.com>
2018-11-19 20:25:24 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,10 +28,3 @@
     end
 end
 
-
-
-ninety_nine_problems = Song.new("99 Problems", "Jay-Z", "Rap")
-mask_off = Song.new("Mask Off", "Future", "Rap")
-dance_macabre = Song.new("Dance Macabre", "Ghost", "Metal")
-if_you_have_ghosts = Song.new("If You Have Ghosts", "Ghost", "Metal")
-Song.genres
