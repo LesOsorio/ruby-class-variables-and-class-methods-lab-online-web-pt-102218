63d6e6e6298eb6f184352b01533fc477e7aabef3
Leslie Osorio <lesosorio17@gmail.com>
2018-11-19 20:42:03 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -44,9 +44,10 @@
     @@artist.each do |artist|
       if artist_count[artist]
         artist_count[artist] += 1
-      else 
+      else
         artist_count[artist] = 1
-      end 
-    end 
-    artist_count  
+      end
+    end
+    artist_count
   end
+end 
