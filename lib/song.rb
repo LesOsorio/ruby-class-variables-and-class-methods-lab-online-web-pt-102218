2ffdffa5e95204672988b2c6f15a02f11fce4307
Leslie Osorio <lesosorio17@gmail.com>
2018-11-19 19:46:37 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,17 +1,4 @@
 
   class Song
-    attr_accessor :name, :artist, :grene
-    @@count = 0
-
-    def initialize
-      @@count =+1
-    end
-    def self.count
-      @@song_count
-    end
-    def genre
-      @@genre = []
-      @@genre << genre
-    end
-
+    
   end
