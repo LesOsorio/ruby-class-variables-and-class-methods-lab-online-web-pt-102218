40ebd9164c75ca6c86ee715178ff907704fb8af6
Leslie Osorio <lesosorio17@gmail.com>
2018-11-19 19:51:12 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,5 +2,14 @@
   class Song
     attr_accessor :name, :artist, :genre
     def initialize(name, artist, genre)
+      ninety_nine_problems = Song.new("99 Problems", "Jay-Z", "rap")
+ 
+      ninety_nine_problems.name
+
+ 
+      ninety_nine_problems.artist
+  
+ 
+      ninety_nine_problems.genre
     end 
   end
